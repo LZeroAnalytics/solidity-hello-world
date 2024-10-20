683de85d9831963e9748f2f88808e197acebf44b
Brian Le <brianle1301@outlook.com>
2024-10-20 15:44:58 +0100

    Update Greeter.sol

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.4;
 
 contract Greeter {
-    string private greeting2;
+    string private greeting;
 
     constructor(string memory _greeting) {
-        greeting2 = _greeting;
+        greeting = _greeting;
     }
 
     function greet() public view returns (string memory) {
-        return greeting2;
+        return greeting;
     }
 
     function setGreeting(string memory _greeting) public {
-        greeting2 = _greeting;
+        greeting = _greeting;
     }
 }
