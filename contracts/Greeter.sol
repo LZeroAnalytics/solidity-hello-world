3ecd7938b96c31efb76ff19434dfa6d5e3a4a815
Brian Le <brianle1301@outlook.com>
2024-10-20 15:30:46 +0100

    Update Greeter.sol

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,14 +5,14 @@ contract Greeter {
     string private greeting;
 
     constructor(string memory _greeting) {
-        greeting = _greeting;
+        greeting1 = _greeting;
     }
 
     function greet() public view returns (string memory) {
-        return greeting;
+        return greeting1;
     }
 
     function setGreeting(string memory _greeting) public {
-        greeting = _greeting;
+        greeting1 = _greeting;
     }
 }
